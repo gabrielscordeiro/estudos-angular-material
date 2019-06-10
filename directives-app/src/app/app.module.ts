2bd25cb89ffe5be733fddb2040ce3e5d41e58869
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2019-06-10 17:35:12 -0300

    Exemplo ngFor

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -14,12 +14,14 @@ import {
   MatInputModule,
   MatListModule,
   MatIconModule,
-  MatDividerModule
+  MatDividerModule,
+  MatButtonModule
 } from '@angular/material';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
 import { NgForComponent } from './ng-for/ng-for.component';
+import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,8 @@ import { NgForComponent } from './ng-for/ng-for.component';
     NgClassComponent,
     NgIfComponent,
     SubNgIfComponent,
-    NgForComponent
+    NgForComponent,
+    NgForFormComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +44,8 @@ import { NgForComponent } from './ng-for/ng-for.component';
     MatInputModule,
     MatListModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
