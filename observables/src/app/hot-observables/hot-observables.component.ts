1f21af5109de57568dda6289329425ed2c1e4f51
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2019-06-29 11:01:46 -0300

    Hot observables

--- observables/src/app/hot-observables/hot-observables.component.ts
+++ observables/src/app/hot-observables/hot-observables.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { Observable, Observer, Subject, ConnectableObservable } from 'rxjs';
+
+import { publish, refCount } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hot-observables',
@@ -15,25 +17,74 @@ export class HotObservablesComponent implements OnInit {
 
   private myObservable: Observable<number>;
 
-
   constructor() { }
 
   ngOnInit() {
     this.myObservable = new Observable((observer: Observer<number>) => {
       let i: number = 0;
 
+      console.log('%c Observable Created', 'background:#ccc;color:#ff0000')
+
       setInterval(() => {
         i++;
-        if (i == 100) {
-          observer.complete();
-        } else {
-          observer.next(i);
-        }
-      }, 1000)
-    })
+        console.log(`%c i = ${i} Observable Created`, 'background:#ccc;color:#0000ff');
+        (i == 100) ? observer.complete() : observer.next(i)
+      }, 1000);
+    });
+
+    //this.usingSubjects();
+    this.usingPublish();
+  }
+
+  usingPublish() {
+    //const multicasted = this.myObservable.pipe(publish(), refCount());
+    const multicasted: ConnectableObservable<number> = this.myObservable.pipe(publish()) as ConnectableObservable<number>;
+
+
+    multicasted.connect();
+
+    setTimeout(() => {
+      multicasted.subscribe((_n) => {
+        this.n1 = _n;
+        this.s1 = 'OK';
+      })
+    }, 2000);
+
+
+    this.s2 = 'Waiting for interval...';
+    //Subscriber 2
+    setTimeout(() => {
+      multicasted.subscribe((_n) => {
+        this.n2 = _n;
+        this.s2 = 'OK';
+      })
+    }, 4000);
   }
 
   usingSubjects() {
+    const subject = new Subject<number>();
+    this.myObservable.subscribe(subject);
+
+    this.s1 = 'Waiting for interval...';
+
+    //Subscriber 1
+    setTimeout(() => {
+      subject.subscribe((_n) => {
+        this.n1 = _n;
+        this.s1 = 'OK';
+      })
+    }, 2000);
+
+
+    this.s2 = 'Waiting for interval...';
+    //Subscriber 2
+    setTimeout(() => {
+      subject.subscribe((_n) => {
+        this.n2 = _n;
+        this.s2 = 'OK';
+      })
+    }, 4000);
+
 
   }
 
